01eb7808d65e61dc01b45c9a8d106895179933fc
= <alfonse.z@gmail.com>
2016-10-24 19:10:13 -0400

    Add scanner

---
+++ scanner.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+  "flag"
+  "time"
+)
+
+func main() {
+  //parallelPtr := flag.Int("parallel", 1, "Amount of parallelization")
+  //blockFilePtr := flag.String("cidrBlockFiles", "cidrs.txt", "CIDR blocks to scan, one per line.")
+  blockPtr := flag.String("cidrBlock", "10.0.0.0/8", "CIDR block to scan")
+  portPtr := flag.Int("port", 23, "Target port")
+  timeoutPtr := flag.Int("timeout", 10, "Time (ms) to wait for established TCP connection")
+  verbosePtr := flag.Bool("v", false, "Print everything")
+
+  flag.Parse()
+
+  scanCidr(blockPtr, portPtr, timeoutPtr, verbosePtr)
+}
+
+func scanCidr(block *string, port *int, timeout *int, verbose *bool) {
+  ip, ipnet, err := net.ParseCIDR(*block)
+  if err != nil {
+    log.Fatal(err)
+  }
+  numip := 0
+  onlineip := 0
+  for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+    conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip, *port), time.Duration(*timeout) * time.Millisecond)
+    if err == nil {
+      fmt.Println(ip, "online")
+      onlineip += 1
+      conn.Close()
+    } else {
+      if(*verbose){
+        fmt.Println(ip, "offline")
+      }
+    }
+    numip += 1
+  }
+
+  fmt.Println("Scanned addresses:", numip)
+  fmt.Println("Online addresses:", onlineip)
+}
+
+func inc(ip net.IP) {
+	for j := len(ip)-1; j>=0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
